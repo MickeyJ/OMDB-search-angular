68d83ed6f9b032ae8fe7d465becbed74a634d213
Mickey <develop.agility@gmail.com>
2016-05-05 13:31:38 -0600

    bundling vendors via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,9 @@ const config = {
       }
     })
   ],
+  resolve: {
+    extensions: ['', '.html', '.js', '.scss', '.css']
+  },
   module: {
     loaders: [
       {
@@ -46,10 +49,18 @@ const config = {
         exclude: /node_modules/
       },
       {
-        test: /\.s?css/,
-        loaders: ["style", "css", "sass"],
+        test: /\.scss/,
+        loader: 'style-loader!css-loader!sass-loader!',
         exclude: /node_modules/
-      }
+      },
+      {
+        test: /\.css?$/, 
+        loader: "style-loader!css-loader!"
+      },
+      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
   }
 };
