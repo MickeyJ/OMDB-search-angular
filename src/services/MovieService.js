5373359c4c9f4eee56215e0e36e0186a3dbebffb
Mickey <develop.agility@gmail.com>
2016-05-04 22:32:12 -0600

    change omdb api to https

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,12 +1,11 @@
 
 const MovieService = ($http) => ({
   getAllMovies(title){
-    return $http.get(`http://www.omdbapi.com/?s=${title}&type=movie`)
+    return $http.get(`https://www.omdbapi.com/?s=${title}&type=movie`)
   },
   getMovie(id){
-    return $http.get(`http://www.omdbapi.com/?i=${id}&type=movie`)
+    return $http.get(`https://www.omdbapi.com/?i=${id}&type=movie`)
   }
 });
-
 
 export default [ '$http', MovieService ]
