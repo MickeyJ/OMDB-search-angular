3df88af2106dd77a0a84e2f4136adff2f1eab5ef
Mickey <develop.agility@gmail.com>
2016-05-04 22:00:54 -0600

    modified MovieService

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,13 +1,12 @@
 
-const MovieService = (
-  ($http) => ({
-    getAllMovies(title){
-      return $http.get(`http://www.omdbapi.com/?s=${title}&type=movie`)
-    },
-    getMovie(id){
-      return $http.get(`http://www.omdbapi.com/?i=${id}&type=movie`)
-    }
-  })
-);
+const MovieService = ($http) => ({
+  getAllMovies(title){
+    return $http.get(`http://www.omdbapi.com/?s=${title}&type=movie`)
+  },
+  getMovie(id){
+    return $http.get(`http://www.omdbapi.com/?i=${id}&type=movie`)
+  }
+});
+
 
 export default [ '$http', MovieService ]
