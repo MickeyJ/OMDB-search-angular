21c42df4f5e4d2e8ce86898209842083182604c9
Mickey <develop.agility@gmail.com>
2016-05-05 11:44:06 -0600

    fixed html fallback route in server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('*', function(req, res, next) {
   res.sendFile('index.html', {
-    root: __dirname + '/../public/'
+    root: __dirname + '/public/'
   });
 });
 
